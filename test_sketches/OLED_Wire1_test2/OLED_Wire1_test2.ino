d1ba5c457e286d6369ef5c5f8c7bdb469dffdc49
millerlp <millerlp@gmail.com>
2020-08-07 11:31:23 -0700

    Updated with new fast sample routine

--- test_sketches/OLED_Wire1_test2/OLED_Wire1_test2.ino
+++ test_sketches/OLED_Wire1_test2/OLED_Wire1_test2.ino
@@ -1,7 +1,8 @@
 /* 2020-07-09 L Miller
  * Basic demonstration of reading multiple MAX30105 sensors and outputting
  * raw values to the OLED display screen (and serial monitor). Tested with
- * heart rate daughterboard revB and Teensy 3.5.
+ * heart rate daughterboard revB and Teensy 3.5. Good for checking functioning
+ * of newly built sensors
  * 
 */
 
@@ -15,6 +16,14 @@
 #define TCAADDR 0x70  // I2C address for the TCA9548 I2C multiplexer
 MAX30105 particleSensor; // create MAX30105 object called particleSensor
 
+// Particle sensor settings
+byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA (full on)
+byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32. We use immediate read, so no point to averaging
+byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green. We use Red+IR for heart stuff
+byte sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+int pulseWidth = 215; //Options: 69, 118, 215, 411. Higher values = more sensitivity
+int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
 #define MULTIPLE_I2C_PORTS 1  // for ssd1306Ascii library, tell it there are multiple I2C ports available
 
 SSD1306AsciiWire oled(Wire1); // create OLED display object, using I2C Wire1 port
@@ -23,7 +32,10 @@ SSD1306AsciiWire oled(Wire1); // create OLED display object, using I2C Wire1 por
 // Create an array to hold numbers of good sensor channels (up to 8)
 byte goodSensors[] = {127,127,127,127,127,127,127,127};
 byte numgoodSensors = 0;
+unsigned int myIntervalMS = 50;  // units milliseconds
+unsigned long myMillis;
 
+//------------------------------------------------------------------------
 void setup() {
   Serial.begin(57600); // Speed doesn't actually matter for Teensy3
   // Start the I2C instance on SCL0/SDA0 of the Teensy3.5 (connected to TCA9548 and heart sensors
@@ -62,17 +74,15 @@ void setup() {
         numgoodSensors++;
       }
     }
-    // Setup to sense a nice looking saw tooth on the plotter
-    byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA
-    byte sampleAverage = 8; //Options: 1, 2, 4, 8, 16, 32
-    byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-    byte sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-    int pulseWidth = 215; //Options: 69, 118, 215, 411
-    int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-  
-    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 
-
+    //Configure sensor with these initial settings
+    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); 
+    particleSensor.disableDIETEMPRDY(); //disable temp ready interrupt.
+    // Tweak individual settings
+    particleSensor.setPulseAmplitudeRed(0x01); // essentially turn off red LED to save power, we only want IR LED.
+    particleSensor.setPulseAmplitudeIR(ledBrightness); // set IR led brightness to user's chosen value 0x00 (off) to 0xFF(full power)
+    particleSensor.setPulseWidth(pulseWidth); //Options: 69, 118, 215, 411. Higher values = more sensitivity
+    
   }
 
   if (numgoodSensors == 0){
@@ -102,21 +112,28 @@ void setup() {
   oled.clear(); 
   oled.home();
   oled.set1X();
+
+  myMillis = millis();
 }  // end of setup loop
 
 //****** main loop ***********
 void loop() {
-
-  for (byte i = 0; i < 8; i++){
-    if (goodSensors[i] != 127) {
-      tcaselect(i);
-      uint32_t sensorVal = particleSensor.getIR();
-      Serial.print(sensorVal); //Send raw data to plotter
-      Serial.print("\t");
-      printSensorOLED(i, sensorVal); // Use function below to print sensor values to OLED screen
-    } 
+  unsigned long currentMillis = millis(); // update currentMillis
+  if ( (currentMillis - myMillis) >= myIntervalMS) {
+      myMillis = currentMillis; // update myMillis
+    for (byte i = 0; i < 8; i++){
+      if (goodSensors[i] != 127) {
+        tcaselect(i);
+  //      uint32_t sensorVal = particleSensor.getIR();
+        uint32_t sensorVal = quickSampleIR();
+        Serial.print(sensorVal); //Send raw data to plotter
+        Serial.print("\t");
+        printSensorOLED(i, sensorVal); // Use function below to print sensor values to OLED screen
+      } 
+    }
+    Serial.println();
   }
-  Serial.println();
+
 }
 
 
@@ -184,3 +201,15 @@ void printSensorOLED(uint8_t i, uint32_t sensorValue){
   }
   
 }
+
+//-----------------------------------------------
+// Custom sampling function for the MAX30105, calling functions in the MAX30105.h library
+uint32_t quickSampleIR(void) {
+  // Clear the MAX30105 FIFO buffer so that there will only be one new sample to read
+  particleSensor.clearFIFO(); 
+  // Multiply pulseWidth by 2 because we always have to wait for the Red LED to sample first
+  // before the IR LED gets sampled. Then account for time taken for any sample averages, and
+  // add on a buffer of 50 more microseconds just for safety's sake
+  delayMicroseconds( (pulseWidth * 2 * sampleAverage) + 50) ;
+  return(particleSensor.getIR());
+} // end of quickSampleIR function
