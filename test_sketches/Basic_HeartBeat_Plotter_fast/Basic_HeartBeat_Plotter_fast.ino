26d3a4a187678be44d0ea849757e58bce6660ffc
Luke Miller <millerlp@gmail.com>
2020-08-21 16:58:14 -0700

    Implementing tighter timing for averaging
    Also added an option to just show the time in milliseconds needed to complete a round of samples from 1 or more sensors.

--- test_sketches/Basic_HeartBeat_Plotter_fast/Basic_HeartBeat_Plotter_fast.ino
+++ test_sketches/Basic_HeartBeat_Plotter_fast/Basic_HeartBeat_Plotter_fast.ino
@@ -8,6 +8,12 @@
  * last most recent value. Both are only executing every 50ms, so in this
  * sketch quick sampling isn't actually any faster than non-quick sample. 
  * 
+ * You can also set QUICKSAMPLE to be 2, in which case it uses the quick sample
+ * routine, but instead outputs the time (in milliseconds) needed to poll the
+ * sensor(s) one time. If NUM_SENSORS is small, this value should be small, and gets
+ * longer as NUM_SENSORS increases, or if you increase sampleAverage value, or 
+ * increase pulseWidth value, or lower the sampleRate value.
+ * 
  * You can also uncomment the #define SLEEP line to enable the shutdown and
  * wakeup functions for the MAX30105. 
  * 
@@ -17,24 +23,29 @@
 #include <Wire.h>
 #include "MAX30105.h"
 
-#define NUM_SENSORS 1
-#define QUICKSAMPLE 1// Set to 1 to enable quick sample, set to 0 to disable quick sample
-#define SLEEP  // Comment this out to disable sleep routine
+#define NUM_SENSORS 8
+#define QUICKSAMPLE 2 // Set 0 to use old sampling method, 1 to enable quick sample, set 2 to show millis() interval
+#define SLEEP  // Comment this out to disable sleep routine. Leave uncommented to use shutdown/wakeup
 
 
 MAX30105 particleSensor;
 // sensor configurations
 byte ledBrightness = 0x1F; //Options: 0=Off to 255=fully on
-byte sampleAverage = 8; //Options: 1, 2, 4, 8, 16, 32
+byte sampleAverage = 2; //Options: 1, 2, 4, 8, 16, 32
 byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-int sampleRate = 1000; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-int pulseWidth = 411; //Options: 69, 118, 215, 411, units microseconds. Applies to all active LEDs
+int sampleRate = 800; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+// For pulseWidth = 411, maximum sampleRate value is 400. For pulseWidth = 215, the maximum 
+// sampleRate value is 800. For pulseWidth = 118, maximum sampleRate value is 1000
+int pulseWidth = 215; //Options: 69, 118, 215, 411, units microseconds. Applies to all active LEDs
 int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
 #define TCAADDR 0x70
 
 unsigned int myIntervalMS = 50;  // units milliseconds
 unsigned long myMillis;
+unsigned long myMillisExtra; 
+
+
 
 void setup()
 {
@@ -72,9 +83,14 @@ void setup()
   }
   baseValue /= avgAmount;
 
+#if (QUICKSAMPLE == 0 | QUICKSAMPLE == 1)
   //Pre-populate the plotter so that the Y scale is close to IR values
-  for (int x = 0 ; x < 500 ; x++)
-    Serial.println(baseValue);
+  if (NUM_SENSORS == 1){
+    for (int x = 0 ; x < 500 ; x++) {
+      Serial.println(baseValue);
+    }
+  }
+#endif
 
   myMillis = millis();  
 }
@@ -91,7 +107,7 @@ void loop()
         // This section only executes if the #define SLEEP line is uncommented above
 #ifdef SLEEP        
         particleSensor.wakeUp(); // Wake up sensor to take sample
-        delay(40); // Give the chip a chance to wake up
+        delayMicroseconds(40); // Give the chip a chance to wake up
 #endif        
 
       // This section uses either the quicksample function or traditional getIR() function
@@ -100,6 +116,10 @@ void loop()
         Serial.print(quickSampleIR()); // custom function, see bottom of this file. This waits long enough to gather one sample
 #elif (QUICKSAMPLE == 0)
         Serial.print(particleSensor.getIR()); // The slow way
+#elif (QUICKSAMPLE == 2)
+      // In this version, use the quickSampleIR() function, but instead show
+      // how long it takes to make the reading, later in the section below
+        quickSampleIR();         
 #endif
 
         // This section only executes if the #define SLEEP line is uncommented above
@@ -107,10 +127,15 @@ void loop()
           particleSensor.shutDown(); // shut down sensor once sample is taken to save power
 #endif
 
-        Serial.println();
-      }
-
-  }
+#if (QUICKSAMPLE == 0 | QUICKSAMPLE == 1)
+      Serial.print("\t");  
+#endif
+      } // end of for loop reading sensors
+#if (QUICKSAMPLE == 2)
+    Serial.print(millis() - myMillis);
+#endif
+      Serial.println();
+  } // end of millis check
 
 }  // end of main loop
 
@@ -123,58 +148,113 @@ void tcaselect(uint8_t i) {
   Wire.endTransmission();
 }
 
-//-----------------------------------------------
+
+
+
 // Custom sampling function for the MAX30105, calling functions in the MAX30105.h library
 uint32_t quickSampleIR(void) {
   // Clear the MAX30105 FIFO buffer so that there will only be one new sample to read
   particleSensor.clearFIFO();
+  uint16_t ledSampleTime; // units will be microseconds
+  
+  
   // Implement a delay for new samples to be collected in the FIFO. If sample averaging
-  // is used, this loop will need to execute multiple times to allow the multiple samples
+  // is used, a loop will need to execute multiple times to allow the multiple samples
   // to be collected
-  for (int i = 0; i < sampleAverage; i++){
-    switch (sampleRate) {
-      case 50:
-        delay(20);
-        break;
-      case 100:
-        delay(10);
-        break;
-      case 200:
-        delay(5);
-        break;
-      case 400:
-        delayMicroseconds(2500);
-        break;
-      case 800:
-        delayMicroseconds(1250);
-        break;
-      case 1000:
-        delay(1);
-        break;
-      case 1600:
-        delayMicroseconds(625);
-        break;
-      case 3200:
-        delayMicroseconds(313);
-        break;
-    }
+  
+  // Start by waiting the maximum time needed for the 1st sample to be taken, based 
+  // on the sample rate
+  switch (sampleRate) {
+    // Options 50, 100, 200, 400, 800, 1000, 1600, 3200 samples per second
+    case 50:
+      delay(20); // units milliseconds
+      break;
+    case 100:
+      delay(10);
+      break;
+    case 200:
+      delay(5);
+      break;
+    case 400:
+      delayMicroseconds(2500);
+      break;
+    case 800:
+      delayMicroseconds(1250);
+      break;
+    case 1000:
+      delay(1);
+      break;
+    case 1600:
+      delayMicroseconds(625);
+      break;
+    case 3200:
+      delayMicroseconds(313);
+      break;
+    default:
+      delay(5);
+      break;
+  }
+   
+  for (int avg = 0; avg < sampleAverage; avg++){
     // Now add in the delay for the actual LED flashes to happen
     switch (pulseWidth) {
-      // 69, 118, 215, 411
+      // options for pulseWidth: 69, 118, 215, 411 microseconds
       case 69:
-        delayMicroseconds( pulseWidth + 427 + 50);
+        ledSampleTime = pulseWidth + 358 + pulseWidth; 
+        delayMicroseconds(ledSampleTime);
         break;
       case 118:
-        delayMicroseconds( pulseWidth + 525 + 50);
+        ledSampleTime = pulseWidth + 407 + pulseWidth;
+        delayMicroseconds(ledSampleTime);
         break;
       case 215:
-        delayMicroseconds( pulseWidth + 720 + 50);
+        ledSampleTime = pulseWidth + 505 + pulseWidth;
+        delayMicroseconds(ledSampleTime);
         break;
       case 411:
-        delayMicroseconds( pulseWidth + 1107 + 50);
+        ledSampleTime = pulseWidth + 696 + pulseWidth;
+        delayMicroseconds(ledSampleTime);
         break;
     }
+    // If more than one sample is being averaged, you need to
+    // further wait for the start of the next sampling cycle,
+    // which is determined by the sampleRate and how long it just
+    // took for the red and IR leds to be sampled. This won't 
+    // execute on the last sample of the average because it isn't
+    // needed after the IR LED is read for the last time
+    if (avg < (sampleAverage - 1)) {
+      switch (sampleRate){
+        case 50:
+          delayMicroseconds(20000 - ledSampleTime);
+          break;
+        case 100:
+          delayMicroseconds(10000 - ledSampleTime);
+          break;
+        case 200:
+          delayMicroseconds(5000 - ledSampleTime);
+          break;
+        case 400:
+          delayMicroseconds(2500 - ledSampleTime);
+          break;
+        case 800:
+          delayMicroseconds(1250 - ledSampleTime);
+          break;
+        case 1000:
+          delayMicroseconds(1000 - ledSampleTime);
+          break;
+        case 1600:
+          delayMicroseconds(625 - ledSampleTime);
+          break;
+        case 3200:
+          delayMicroseconds(313 - ledSampleTime);
+          break;
+        default:
+          delayMicroseconds(5000 - ledSampleTime);
+          break;     
+      } // end of switch statement
+    } // end of if statement
   } // end up of delay loop
+  delayMicroseconds(50); // Add in a little extra delay just to be safe
   // Query the FIFO buffer on the sensor for the most recent IR value
   return (particleSensor.getIR());
 } // end of quickSampleIR function
