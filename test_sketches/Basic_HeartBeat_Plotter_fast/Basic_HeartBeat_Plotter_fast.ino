f976307a0f86eeb97d7062ebf3c1f239c4d3b804
Luke Miller <millerlp@gmail.com>
2020-08-20 22:56:29 -0700

    Updated to fix noise issue
    Got rid of the setPulseWidth() call, which was apparently screwing everything up. Pulse width is already set in the particleSensor.setup() function anyhow

--- test_sketches/Basic_HeartBeat_Plotter_fast/Basic_HeartBeat_Plotter_fast.ino
+++ test_sketches/Basic_HeartBeat_Plotter_fast/Basic_HeartBeat_Plotter_fast.ino
@@ -9,8 +9,8 @@
  * sketch quick sampling isn't actually any faster than non-quick sample. 
  * 
  * You can also uncomment the #define SLEEP line to enable the shutdown and
- * wakeup functions for the MAX30105. Currently these seem to bork the data
- * from the sensor, just giving oscillating values. 
+ * wakeup functions for the MAX30105. 
+ * 
 
 */
 
@@ -19,15 +19,15 @@
 
 #define NUM_SENSORS 1
 #define QUICKSAMPLE 1// Set to 1 to enable quick sample, set to 0 to disable quick sample
-//#define SLEEP  // Comment this out to disable sleep routine
+#define SLEEP  // Comment this out to disable sleep routine
 
 
 MAX30105 particleSensor;
 // sensor configurations
 byte ledBrightness = 0x1F; //Options: 0=Off to 255=fully on
-byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
+byte sampleAverage = 8; //Options: 1, 2, 4, 8, 16, 32
 byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-byte sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+int sampleRate = 1000; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
 int pulseWidth = 411; //Options: 69, 118, 215, 411, units microseconds. Applies to all active LEDs
 int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
@@ -40,6 +40,8 @@ void setup()
 {
   Serial.begin(115200);
   while(!Serial); // wait for serial port to connect
+  Wire.setSCL(19); // Teensy3.5 Wire SCL pin on daughterboard RevA
+  Wire.setSDA(18); // Teensy3.5 Wire SDA pin on daughterboard RevA
   Wire.begin();
 
   //Sensor setup
@@ -50,11 +52,11 @@ void setup()
     if (particleSensor.begin(Wire, I2C_SPEED_FAST)) { // connect sensor to I2C bus
       particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 //      particleSensor.enableDIETEMPRDY(); //enable temp ready interrupt. Required to log temp, but each read takes 29ms
-      particleSensor.disableDIETEMPRDY(); //disable temp ready interrupt.
+//      particleSensor.disableDIETEMPRDY(); //disable temp ready interrupt.
       // Tweak individual settings
-      //particleSensor.setPulseAmplitudeRed(0x00); // essentially turn off red LED to save power, we only want IR LED. **** commented for testing only
+      particleSensor.setPulseAmplitudeRed(0x00); // essentially turn off red LED to save power, we only want IR LED. **** commented for testing only
       particleSensor.setPulseAmplitudeIR(ledBrightness); // set IR led brightness to user's chosen value 0x00 (off) to 0xFF(full power)
-      particleSensor.setPulseWidth(pulseWidth); //Options: 69, 118, 215, 411. Higher values = more sensitivity
+//      particleSensor.setPulseWidth(pulseWidth); //DANGER - this seems to screw things up! Options: 69, 118, 215, 411. Higher values = more sensitivity
 //      triggerTemperatureSample(); // Start the temperature sample (wait 29 ms before attempting to read)
     }
   }
@@ -82,20 +84,25 @@ void loop()
 {
   if ( (millis() - myMillis) >= myIntervalMS) {
     myMillis = millis();
-//    Serial.println(particleSensor.getIR()); //Send raw data to plotter
+
     for (byte i = 0; i < NUM_SENSORS; i++) {
-        tcaselect(i);
+        tcaselect(i); // choose sensor channel
+
+        // This section only executes if the #define SLEEP line is uncommented above
 #ifdef SLEEP        
         particleSensor.wakeUp(); // Wake up sensor to take sample
         delay(40); // Give the chip a chance to wake up
 #endif        
 
+      // This section uses either the quicksample function or traditional getIR() function
+      // depending on the value assigned to #define QUICKSAMPLE up above
 #if (QUICKSAMPLE == 1)        
         Serial.print(quickSampleIR()); // custom function, see bottom of this file. This waits long enough to gather one sample
 #elif (QUICKSAMPLE == 0)
         Serial.print(particleSensor.getIR()); // The slow way
 #endif
-        
+
+        // This section only executes if the #define SLEEP line is uncommented above
 #ifdef SLEEP
           particleSensor.shutDown(); // shut down sensor once sample is taken to save power
 #endif
@@ -103,24 +110,9 @@ void loop()
         Serial.println();
       }
 
-   
   }
 
-}
-
-
-//-----------------------------------------------
-// Custom sampling function for the MAX30105, calling functions in the MAX30105.h library
-uint32_t quickSampleIR(void) {
-  // Clear the MAX30105 FIFO buffer so that there will only be one new sample to read
-  particleSensor.clearFIFO();
-  // Multiply pulseWidth by 2 because we always have to wait for the Red LED to sample first
-  // before the IR LED gets sampled. Also account for time taken for any sample averages.
-  // Then add on 696us for the delay between two LED channels ("slot timing", see datasheet Table 14)
-  // and add on a buffer of 50 more microseconds just for safety's sake
-  delayMicroseconds( (pulseWidth * 2 * sampleAverage) + 696 + 50) ;
-  return (particleSensor.getIR());
-} // end of quickSampleIR function
+}  // end of main loop
 
 
 //******************************
@@ -131,30 +123,58 @@ void tcaselect(uint8_t i) {
   Wire.endTransmission();
 }
 
-
-//---------------------------------------------
-// Set up to read Die Temperature
-// After calling this function, wait at least 29 milliseconds before
-// querying for a temperature value with the readTemperatureSample() function
-void triggerTemperatureSample(void) {
-  // Remember: DIE_TEMP_RDY interrupt must be enabled to read temperatures
-  //See issue 19: https://github.com/sparkfun/SparkFun_MAX3010x_Sensor_Library/issues/19
-  // MAX30105 I2C address is 0x57 (always)
-  // MAX30105_DIETEMPCONFIG register address is 0x21
-  // And send 0x01 to enable a single temperature read
-  particleSensor.writeRegister8(0x57, 0x21, 0x01);
-}
-
-//-----------------------------------
-// Check for new die temperature reading
-// This function should be run at least 29 milliseconds after calling
-// the triggerTemperatureSample() function which starts the temperature sample
-
-float readTemperatureSample(void) {
-  // Read die temperature register (integer)
-  int8_t tempInt = particleSensor.readRegister8(0x57, 0x1F);
-  uint8_t tempFrac = particleSensor.readRegister8(0x57, 0x20); //Causes the clearing of the DIE_TEMP_RDY interrupt
-
-  // Calculate temperature (datasheet pg. 23)
-  return (float)tempInt + ((float)tempFrac * 0.0625);
-}
+//-----------------------------------------------
+// Custom sampling function for the MAX30105, calling functions in the MAX30105.h library
+uint32_t quickSampleIR(void) {
+  // Clear the MAX30105 FIFO buffer so that there will only be one new sample to read
+  particleSensor.clearFIFO();
+  // Implement a delay for new samples to be collected in the FIFO. If sample averaging
+  // is used, this loop will need to execute multiple times to allow the multiple samples
+  // to be collected
+  for (int i = 0; i < sampleAverage; i++){
+    switch (sampleRate) {
+      case 50:
+        delay(20);
+        break;
+      case 100:
+        delay(10);
+        break;
+      case 200:
+        delay(5);
+        break;
+      case 400:
+        delayMicroseconds(2500);
+        break;
+      case 800:
+        delayMicroseconds(1250);
+        break;
+      case 1000:
+        delay(1);
+        break;
+      case 1600:
+        delayMicroseconds(625);
+        break;
+      case 3200:
+        delayMicroseconds(313);
+        break;
+    }
+    // Now add in the delay for the actual LED flashes to happen
+    switch (pulseWidth) {
+      // 69, 118, 215, 411
+      case 69:
+        delayMicroseconds( pulseWidth + 427 + 50);
+        break;
+      case 118:
+        delayMicroseconds( pulseWidth + 525 + 50);
+        break;
+      case 215:
+        delayMicroseconds( pulseWidth + 720 + 50);
+        break;
+      case 411:
+        delayMicroseconds( pulseWidth + 1107 + 50);
+        break;
+    }
+  } // end up of delay loop
+  // Query the FIFO buffer on the sensor for the most recent IR value
+  return (particleSensor.getIR());
+} // end of quickSampleIR function
