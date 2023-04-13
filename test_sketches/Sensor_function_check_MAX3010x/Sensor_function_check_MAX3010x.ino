883e018d1c6333e797d5e38233154105264ae916
millerlp <millerlp@gmail.com>
2023-04-12 20:44:22 -0700

    Cleaning up the intro

--- test_sketches/Sensor_function_check_MAX3010x/Sensor_function_check_MAX3010x.ino
+++ test_sketches/Sensor_function_check_MAX3010x/Sensor_function_check_MAX3010x.ino
@@ -6,24 +6,26 @@
 */
 
 #include <Wire.h>
-#include "MAX30105.h" // https://github.com/sparkfun/SparkFun_MAX3010x_Sensor_Library
-#include "heartRate.h" // https://github.com/sparkfun/SparkFun_MAX3010x_Sensor_Library
+#include "MAX30105.h" // https://github.com/millerlp/SparkFun_MAX3010x_Sensor_Library
+#include "heartRate.h" // https://github.com/millerlp/SparkFun_MAX3010x_Sensor_Library
 #include "SSD1306Ascii.h" // https://github.com/greiman/SSD1306Ascii
 #include "SSD1306AsciiWire.h" // https://github.com/greiman/SSD1306Ascii
 
+#define TCAADDR 0x70  // I2C address for the TCA9548 I2C multiplexer, used to select MAX3010x sensors
 
-#define TCAADDR 0x70  // I2C address for the TCA9548 I2C multiplexer
-//MAX30105 particleSensor; // create MAX30105 object called particleSensor
+// MAX3010x sensor settings
 MAX30105 max3010x; // create MAX3010x object
-
-// Particle sensor settings
-byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA (full on)
+#define MAX_SENSORS 8  // Leave this set at 8, even if fewer than 8 sensors are attached
 byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32. We use immediate read, so no point to averaging
 byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green. We use Red+IR for heart stuff
 byte sampleRate = 800; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
 int pulseWidth = 215; //Options: 69, 118, 215, 411. Higher values = more sensitivity
 int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-
+// Define IR led brightness setting for each of the 8 channels
+// Options: 0=off to 255=fully on, try 10-30 initially. Too high will make noisy signal
+//          Channel =      1   2   3   4   5   6   7   8
+byte IRledBrightness[] = {30, 30, 30, 30, 30, 30, 30, 30};
+byte REDledBrightness = 1; // low value of 0 shuts it off, 1 is barely on
 #define MULTIPLE_I2C_PORTS 1  // for ssd1306Ascii library, tell it there are multiple I2C ports available
 
 SSD1306AsciiWire oled(Wire1); // create OLED display object, using I2C Wire1 port
@@ -55,37 +57,44 @@ void setup() {
   oled.print("Hello");
   oled.println();
   delay(1000);
-  
-  for (byte i = 0; i < 8; i++){
-      // Set the TCA I2C multiplexer to channel 0
-    tcaselect(i);
-    delayMicroseconds(20);
-//    if (max3010x.begin(Wire, 400000)) // Fast I2C, only works with short sensor leads (<5m)
-    // Use default I2C port, I2C_SPEED_STANDARD (100kHz) to accomodate sensors
-    // on the end of very long wire leads (tested up to 10 meters)
-    if (max3010x.begin(Wire, I2C_SPEED_STANDARD)) 
-    {
-      // If sensor is present, mark it in the goodSensors array
-      goodSensors[i] = i;
-      numgoodSensors++;
-    } else {
-      delay(5);
-      // Try a second time to see if the sensor shows up
-      if(max3010x.begin(Wire, I2C_SPEED_STANDARD)){        
-        goodSensors[i] = i;
-        numgoodSensors++;
-      }
-    }
 
-    //Configure sensor with these initial settings
-    max3010x.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); 
-    max3010x.disableDIETEMPRDY(); //disable temp ready interrupt.
-    // Tweak individual settings
-    max3010x.setPulseAmplitudeRed(0x01); // essentially turn off red LED to save power, we only want IR LED.
-    max3010x.setPulseAmplitudeIR(ledBrightness); // set IR led brightness to user's chosen value 0x00 (off) to 0xFF(full power)
-    max3010x.setPulseWidth(pulseWidth); //Options: 69, 118, 215, 411. Higher values = more sensitivity
-    
-  }
+  //*******************************
+  // MAX30105 Sensor setup. This function will scan for avaialble
+  // sensors, and set sampling parameters for those that are found
+  // This will also store which channels are working, so that only
+  // those channels are sampled and stored in the data files
+  scanSetupSensors(); // See function near bottom of this file
+  
+//  for (byte i = 0; i < 8; i++){
+//      // Set the TCA I2C multiplexer to channel 0
+//    tcaselect(i);
+//    delayMicroseconds(20);
+////    if (max3010x.begin(Wire, 400000)) // Fast I2C, only works with short sensor leads (<5m)
+//    // Use default I2C port, I2C_SPEED_STANDARD (100kHz) to accomodate sensors
+//    // on the end of very long wire leads (tested up to 10 meters)
+//    if (max3010x.begin(Wire, I2C_SPEED_STANDARD)) 
+//    {
+//      // If sensor is present, mark it in the goodSensors array
+//      goodSensors[i] = i;
+//      numgoodSensors++;
+//    } else {
+//      delay(5);
+//      // Try a second time to see if the sensor shows up
+//      if(max3010x.begin(Wire, I2C_SPEED_STANDARD)){        
+//        goodSensors[i] = i;
+//        numgoodSensors++;
+//      }
+//    }
+//
+//    //Configure sensor with these initial settings
+//    max3010x.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); 
+//    max3010x.disableDIETEMPRDY(); //disable temp ready interrupt.
+//    // Tweak individual settings
+//    max3010x.setPulseAmplitudeRed(0x01); // essentially turn off red LED to save power, we only want IR LED.
+//    max3010x.setPulseAmplitudeIR(ledBrightness); // set IR led brightness to user's chosen value 0x00 (off) to 0xFF(full power)
+//    max3010x.setPulseWidth(pulseWidth); //Options: 69, 118, 215, 411. Higher values = more sensitivity
+//    
+//  }
 
   if (numgoodSensors == 0){
     oled.println("No sensors");
@@ -93,22 +102,19 @@ void setup() {
     Serial.println("No sensors found");
     while(1);
   } else {
-
-
       // Print available sensors
-      for (byte i = 0; i < 8; i++){
-        if (goodSensors[i] != 127) {
-          Serial.print("Sensor");
-          Serial.print(goodSensors[i]+1); // Start labeling at 1 instead of 0
-          Serial.print("\t");
-          
-        }
+    for (byte i = 0; i < 8; i++){
+      if (goodSensors[i] != 127) {
+        Serial.print("Sensor");
+        Serial.print(goodSensors[i]+1); // Start labeling at 1 instead of 0
+        Serial.print("\t");
       }
-      Serial.println();
-      oled.print("Found ");
-      oled.println(numgoodSensors);
-      oled.println("sensors");
-      delay(2000);
+    }
+    Serial.println();
+    oled.print("Found ");
+    oled.println(numgoodSensors);
+    oled.println("sensors");
+    delay(2000);
   }
 
   oled.clear(); 
@@ -207,18 +213,48 @@ void printSensorOLED(uint8_t i, uint32_t sensorValue){
 }
 
 //-----------------------------------------------
-// Custom sampling function for the MAX30105, calling functions in the MAX30105.h library
-//uint32_t quickSampleIR(void) {
-//  // Clear the MAX30105 FIFO buffer so that there will only be one new sample to read
-//  max3010x.clearFIFO(); 
-//  // Multiply pulseWidth by 2 because we always have to wait for the Red LED to sample first
-//  // before the IR LED gets sampled. Then account for time taken for any sample averages, and
-//  // add on a buffer of 50 more microseconds just for safety's sake
-//  delayMicroseconds( (pulseWidth * 2 * sampleAverage) + 50) ;
-//  return(max3010x.getIR());
-//} // end of quickSampleIR function
+//-------------------------------------------------------------
+// A function to scan for available MAX30105 sensors
+// Saves a record of which sensors returned a valid signature (goodSensors[])
+// Saves a count of the number of good sensors (numgoodSensors)
+// Sets up each sensor using the user's parameters defined at the top of this program
+
+void scanSetupSensors (void) {
+  numgoodSensors = 0; // reset to zero each time this function is called
+  for (byte i = 0; i < MAX_SENSORS; i++) {
+    tcaselect(i);
+    delayMicroseconds(20);
+    goodSensors[i] = 127; // Reset this value before scanning for the sensor
+    if (max3010x.begin(Wire, I2C_SPEED_STANDARD)) //Use default I2C port, 100kHz speed
+    {
+      // If sensor is present, mark it in the goodSensors array
+      goodSensors[i] = i;
+      numgoodSensors++;
+    } else {
+      // If sensor didn't show up, wait a bit and try a 2nd time
+      delay(5);
+      if (max3010x.begin(Wire, I2C_SPEED_STANDARD)) {
+        goodSensors[i] = i;
+        numgoodSensors++;
+      }
+    }
+    // If the sensor was marked good, set it up for our sampling needs
+    if (goodSensors[i] != 127) {
+      max3010x.setup(IRledBrightness[i], sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+      max3010x.enableDIETEMPRDY(); //enable temp ready interrupt. Required to log temp, but each read takes 29ms
+      // Tweak individual settings
+      max3010x.setPulseAmplitudeRed(REDledBrightness); // essentially turn off red LED to save power, we only want IR LED. **** commented for testing only
+      max3010x.setPulseAmplitudeIR(IRledBrightness[i]); // set IR led brightness to user's chosen value 0x00 (off) to 0xFF(full power)
+      // TEST =  disable all Slots, then enable IR led on Slot 1
+//      max3010x.disableSlots();
+//      max3010x.enableSlot(1, 0x02);  // 0x02 represents the IR LED (LED2, page 21 of MAX30102 data sheet)
+//      max3010x.enableSlot(2, 0x00); // 0x00 means shut off the LED
+    }
+  }
+}
 
 
+//---------------------------------------------------------------------------------------------
 
 // Custom sampling function for the MAX30105, calling functions in the MAX30105.h library
 uint32_t quickSampleIR(void) {
